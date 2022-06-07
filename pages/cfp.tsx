a0e65a3df9c2fa6722869ff9044898493110ad2d
Felipe Torres <ftorres@brex.com>
2022-06-07 09:27:32 -0700

    update

--- pages/cfp.tsx
+++ pages/cfp.tsx
@@ -44,7 +44,7 @@ const OnSitePage: NextPage<PageProps> = (props) => {
         {props.heroData && <BannerCFP {...props.heroData} />}
         {props.followUsData && <FollowUsSection page={props.followUsData} />}
       </Container>
-      {props.footerData && <Footer footerData={props.footerData} />}
+      {props.footerData && <Footer page={props.footerData} />}
     </StyledBlackWrapp>
   );
 };
