962744de632b9d209f1e90d7478fe3a4a2f1ff73
Pillippa Perez Pons <ignacia.pp@gmail.com>
2022-06-11 22:22:01 -0700

    [page]: added 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,9 @@
 import type { NextPage } from "next";
 import styled from "@emotion/styled";
+import Image from "../src/Components/core/Image";
+import { H1, P } from "../src/Components/core/Typography";
+import { SecondaryButton } from "../src/Components/Button";
+import { PrimaryStyledLink } from "../src/Components/Links";
 
 export type PageProps = {};
 const Container = styled.section`
@@ -9,14 +13,27 @@ const Container = styled.section`
 const StyledBlackWrapp = styled.section`
   display: flex;
   flex-direction: column;
-  min-height: calc(100vh - 200px);
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh);
+  gap: 32px 0px;
   background-color: ${({ theme }) => theme.elements.global.backgroundColor};
 `;
 
 const Home: NextPage<PageProps> = (props) => {
   return (
     <Container>
-      <StyledBlackWrapp>Lo sentimos!</StyledBlackWrapp>
+      <StyledBlackWrapp>
+        <Image mobile="/images/404.svg" alt="" />
+        <H1>OOPS!</H1>
+
+        <P>
+          <b>JSconf</b> es un evento Open Source, sin fines de lucro, realizado
+          por la comunidad y para la comunidad.
+        </P>
+
+        <PrimaryStyledLink href="/">GO HOME</PrimaryStyledLink>
+      </StyledBlackWrapp>
     </Container>
   );
 };
