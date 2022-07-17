45a849fb96649b0b801f4f726be4695286b669c0
Pillippa Perez Pons <ignacia.pp@gmail.com>
2022-07-17 14:15:48 -0700

    fixed sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://dev.jsconf.cl",
+  siteUrl: process.env.SITE_URL || "https://jsconf.cl",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
