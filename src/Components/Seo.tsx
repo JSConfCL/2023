3fe3fdd68b922c979c7ac3d82aa93296b53ac57d
Jose Lezama <me@joseglego.io>
2022-09-09 12:49:50 -0400

    feat(web.manifest): update favicon, theme_color & background_color

--- src/Components/Seo.tsx
+++ src/Components/Seo.tsx
@@ -16,12 +16,29 @@ const Seo = (props: {
     <>
       <Head>
         <title>{props.title}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content={props.description} />
         {props.metadata?.map((elem, index) => (
           <meta key={`metadata-${index}`} {...elem} />
         ))}
-
         {props.scripts}
       </Head>
     </>
