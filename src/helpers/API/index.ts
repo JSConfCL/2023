dbaa0b2939c91cecf456181641276ec1da92b05b
fforres <felipe.torressepulveda@gmail.com>
2022-10-16 14:28:21 -0700

    Adding tickets UI

--- src/helpers/API/index.ts
+++ src/helpers/API/index.ts
@@ -4,7 +4,22 @@ export const queryClient = new QueryClient();
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const fetchTickets = async () => {
+export const fetchTickets = async (): Promise<
+  Array<{
+    createdAt: string;
+    description: string;
+    id: string;
+    name: string;
+    price: number;
+    priceUSD: number;
+    quantity: number;
+    season: string;
+    status: string;
+    stripePriceId: string;
+    type: string;
+    updatedAt: string;
+  }>
+> => {
   const res = await fetch(`${API_URL}/tickets`);
   return res.json();
 };
