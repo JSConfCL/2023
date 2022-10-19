580bb3e6b940826badae4cea0ca3a28dda467326
Felipe Torres <ftorres@brex.com>
2022-10-19 16:07:05 -0700

    Agregando mejoras de tipos

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,5 +1,3 @@
-type DeepRequired<T> = { [K in keyof T]: DeepRequired<T[K]> } & Required<T>;
-type DeepNonNullable<T> = {
-  [K in keyof T]: DeepNonNullable<T[K]>;
-} & NonNullable<T>;
+import { DeepRequired, DeepNonNullable } from "ts-essentials";
+
 export type ParseQuery<T> = DeepRequired<DeepNonNullable<T>>;
