f2d59caae2cb1fb8631adfec1ebc3916cbf35b46
Pillippa Ignacia Pérez Pons <ignacia.pp@gmail.com>
2022-11-26 16:23:54 -0800

    [FAQ]: added page Frequent questions (#99)

--- src/Components/CustomMarkdown/index.tsx
+++ src/Components/CustomMarkdown/index.tsx
@@ -1,6 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { H1, H2, P } from "../core/Typography";
+import { H1, H2, H3, P } from "../core/Typography";
 import styled from "@emotion/styled";
 import { ViewportSizes } from "../../../styles/theme";
 import { transparentize } from "polished";
@@ -28,6 +28,34 @@ const StyledH2 = styled(H2)`
   @media (min-width: ${ViewportSizes.Desktop}px) {
     font-size: 4rem;
   }
+`;
+
+const StyledH3 = styled(H3)`
+  margin-top: 32px;
+`;
+
+const Table = styled.table`
+  padding: 16px;
+
+  td,
+  th {
+    padding: 16px;
+    border: ${({ theme }) => `1px solid ${theme.colors.jsconfYellow}`};
+  }
+  th {
+    border: 0px;
+    color: ${({ theme }) => theme.colors.jsconfBlack};
+    font-weight: 600;
+    background: ${({ theme }) => theme.colors.jsconfYellow};
+  }
+
+  th:last-child {
+    border-radius: 0px 20px 0px 0px;
+  }
+`;
+
+export const Bold = styled.p`
+  font-weight: 700;
 `;
 
 export const Anchor = styled.a`
@@ -69,8 +97,11 @@ export const CustomMarkdown = (props: Props) => {
       components={{
         h1: (args: any) => <StyledH1 {...args} />,
         h2: (args: any) => <StyledH2 {...args} />,
+        h3: (args: any) => <StyledH3 {...args} />,
         p: (args: any) => <P {...args} />,
         a: (args: any) => <Anchor {...args} />,
+        strong: (args: any) => <Bold {...args} />,
+        table: (args: any) => <Table {...args} />,
       }}
     >
       {props.children}
