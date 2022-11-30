796a0a6d0c5a0e754620dbae425e1f03f898d112
Jose Lezama <me@joseglego.io>
2022-11-29 22:49:15 -0300

    chore(page.Settings): replace inner Alert by Common.App.Alert

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
 import { useQuery } from "@tanstack/react-query";
-import { transparentize } from "polished";
 
+import { Alert } from "../src/Components/common/app";
 import { TicketsLayout } from "../src/Components/Layouts/TicketsLayout";
 import { UserInformationForm } from "../src/Components/UserInformationForm";
+
 import { me } from "../src/helpers/API";
 import { jsconfTheme, ViewportSizes } from "../styles/theme";
 
@@ -46,31 +47,17 @@ const StyledLoadingImage = styled.div`
   margin: 0 auto;
 `;
 
-const StyledBanner = styled.div`
-  border-radius: 8px;
-  background: ${transparentize(0.5, jsconfTheme.colors.jsconfRed)};
-  padding: 16px 24px 24px 24px;
-  margin-bottom: 16px;
-
-  > h2 {
-    text-align: center;
-    margin-bottom: 8px;
-    font-size: 1.2em;
-  }
-`;
-
 const Settings = () => {
   const { isLoading, data: user } = useQuery(["me"], me);
   return (
     <StyledContainer>
       {user?.id && !user.email ? (
-        <StyledBanner>
-          <h2>Informacion Importante:</h2>
+        <Alert title="Informacion Importante:">
           Pudimos crear tu cuenta. Pero no conseguimos correo electrónico
           asociado. Por lo tanto, no podremos comunicarnos contigo. Te pedimos
           registres un correo electrónico para poder informarte los próximos
           pasos.
-        </StyledBanner>
+        </Alert>
       ) : null}
       <Title>Configuracion</Title>
       {isLoading ? (
