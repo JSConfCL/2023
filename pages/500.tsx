7773dd040c7705f4f8faa39aeb54ad8cd14effc2
felipe.torressepulveda@gmail.com <ftorres@brex.com>
2022-12-03 22:56:17 -0800

    update

---
+++ pages/500.tsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+import { ErrorComponent } from "../src/Components/ErrorComponent";
+import { DefaultPagelayout } from "../src/Components/Layouts/DefaultPagelayout";
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding-top: 5rem;
+  min-height: 60vh;
+  justify-content: center;
+`;
+
+export default function Page404() {
+  return (
+    <Container>
+      <ErrorComponent errorMessage="La página que estas buscando no existe." />
+    </Container>
+  );
+}
+
+Page404.getLayout = DefaultPagelayout;
--- pages/p/ticket/[id].tsx
+++ pages/p/ticket/[id].tsx
@@ -1,3 +1,5 @@
+import styled from "@emotion/styled";
+import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
 
 import { DefaultPagelayout } from "../../../src/Components/Layouts/DefaultPagelayout";
@@ -9,6 +11,10 @@ export const config = {
   runtime: "experimental-edge",
 };
 
+const StyledSpacer = styled.div(({ theme }) => ({
+  height: 100,
+}));
+
 const TicketPage = ({ ticket }: { ticket: PublicTicket }) => {
   return (
     <div>
@@ -19,15 +25,15 @@ const TicketPage = ({ ticket }: { ticket: PublicTicket }) => {
           content="Mi Ticket para ir a la JSConf, unete a la primera "
         />
 
-        <meta property="og:url" content="https://www.jsconf.cl/" />
+        <meta property="og:url" content="https://www.jsconf.cl/tickets" />
         <meta property="og:type" content="website" />
         <meta
           property="og:title"
-          content={`Ticket: ${ticket.username} | JSConf CL`}
+          content={`${ticket.username} 💛 JSConf Chile`}
         />
         <meta
           property="og:description"
-          content="Mi Ticket para ir a la JSConf, unete a la primera "
+          content="Mi Ticket para ir a la JSConf. Únete a la primera!"
         />
         <meta
           property="og:image"
@@ -36,20 +42,21 @@ const TicketPage = ({ ticket }: { ticket: PublicTicket }) => {
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="www.jsconf.cl" />
-        <meta property="twitter:url" content="https://www.jsconf.cl/" />
+        <meta property="twitter:url" content="https://www.jsconf.cl/tickets" />
         <meta
           name="twitter:title"
-          content={`Ticket: ${ticket.username} | JSConf CL`}
+          content={`${ticket.username} 💛 JSConf Chile`}
         />
         <meta
           name="twitter:description"
-          content="Mi Ticket para ir a la JSConf, unete a la primera "
+          content="Mi Ticket para ir a la JSConf! Únete a la primera!"
         />
         <meta
           name="twitter:image"
           content={`https://tickets-images-worker-localhost.jsconfcl.workers.dev/ticket/image/${ticket.ticketId}`}
         />
       </Head>
+      <StyledSpacer />
       <Ticket
         userTicketId={ticket.ticketId}
         userTicketStatus={ticket.status}
@@ -64,16 +71,35 @@ const TicketPage = ({ ticket }: { ticket: PublicTicket }) => {
   );
 };
 
-export async function getServerSideProps({ query }: { query: { id: string } }) {
+export const getServerSideProps = async ({
+  query,
+  res,
+  req,
+  resolvedUrl,
+}: GetServerSidePropsContext) => {
   const { id } = query;
-  const res = await fetch(
+  if (!id || Array.isArray(id)) {
+    return {
+      redirect: {
+        destination: "/tickets",
+        permanent: false,
+      },
+    };
+  }
+  const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/tickets/qr/info/user_ticket_${id}`
   );
-  const ticket = await res.json();
-
-  console.log({ status: "PROPS", ticket });
+  const ticket = await response.json();
+  if (ticket.statusCode === 500) {
+    return {
+      redirect: {
+        destination: "/tickets",
+        permanent: false,
+      },
+    };
+  }
   return { props: { ticket } };
-}
+};
 
 TicketPage.getLayout = DefaultPagelayout;
 
