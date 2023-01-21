a9e067becf1098d9a2d403de94377aad21bd29d9
Jose Lezama <me@joseglego.io>
2023-01-21 20:16:09 -0300

    197/link tree (#199)
    ## Summary
    - Define a custom link-tree for the conference
    - Extra: Hacky redirect, buttons on navbar are redirects where content
    is the `#anchor-id`. e.g: http://localhost:3001/link-tree#LightningTalks
     
    ## Media
    ### Screenshots
    
    ![197_desktop](https://user-images.githubusercontent.com/238259/213593161-49feab8b-959f-4855-8cdb-4f2c5b2bbaf0.png)
    
    ![197_mobile](https://user-images.githubusercontent.com/238259/213593167-d0a38e8a-898e-4af8-b1ff-1455c0c7a81c.png)
    
    ## Closes
    Closes #197

---
+++ src/graphql/link-tree.generated.tsx
@@ -0,0 +1,100 @@
+import gql from "graphql-tag";
+import * as Urql from "urql";
+
+import * as Types from "../types";
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type LinkTreeQueryVariables = Types.Exact<{
+  locale: Types.Scalars["String"];
+  isPreview?: Types.InputMaybe<Types.Scalars["Boolean"]>;
+}>;
+
+export type LinkTreeQuery = {
+  __typename?: "Query";
+  page?: {
+    __typename?: "Page";
+    seo?: {
+      __typename?: "Seo";
+      title?: string | null;
+      description?: string | null;
+      metadata?: any | null;
+    } | null;
+    heroBlock?: {
+      __typename?: "HeroBlock";
+      tile?: string | null;
+      firstSubtitle?: string | null;
+      secondSubtitle?: string | null;
+    } | null;
+    navBar?: {
+      __typename?: "NavigationBar";
+      description?: {
+        __typename?: "NavigationBarDescription";
+        json: any;
+      } | null;
+      linksCollection?: {
+        __typename?: "NavigationBarLinksCollection";
+        items: Array<{
+          __typename?: "LinkItem";
+          contenido?: string | null;
+          link?: string | null;
+          isBlank?: boolean | null;
+          sys: { __typename?: "Sys"; id: string };
+        } | null>;
+      } | null;
+      buttonsCollection?: {
+        __typename?: "NavigationBarButtonsCollection";
+        items: Array<{
+          __typename?: "LinkItem";
+          contenido?: string | null;
+          link?: string | null;
+        } | null>;
+      } | null;
+    } | null;
+  } | null;
+};
+
+export const LinkTreeDocument = gql`
+  query LinkTree($locale: String!, $isPreview: Boolean = false) {
+    page(id: "3yNee3jJ5xkHkCPyszAiOp", locale: $locale, preview: $isPreview) {
+      seo {
+        title
+        description
+        metadata
+      }
+      heroBlock {
+        tile
+        firstSubtitle
+        secondSubtitle
+      }
+      navBar {
+        description {
+          json
+        }
+        linksCollection(limit: 20) {
+          items {
+            sys {
+              id
+            }
+            contenido
+            link
+            isBlank
+          }
+        }
+        buttonsCollection(limit: 20) {
+          items {
+            contenido
+            link
+          }
+        }
+      }
+    }
+  }
+`;
+
+export function useLinkTreeQuery(
+  options: Omit<Urql.UseQueryArgs<LinkTreeQueryVariables>, "query">
+) {
+  return Urql.useQuery<LinkTreeQuery, LinkTreeQueryVariables>({
+    query: LinkTreeDocument,
+    ...options,
+  });
+}
