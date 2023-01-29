c6c74b4482c371bf9a82a403fc8d75614254be63
Jose Lezama <me@joseglego.io>
2023-01-29 17:27:44 -0300

    Feat/workshops n events comments 9 (#219)
    ## Summary
    - Take into consideration if it's Workshop, Meetup  or JSConf Ticket:
       - Apply Theme
       - Show custom information
    
    ## Media
    ### Screenshots
    
    ![image](https://user-images.githubusercontent.com/238259/215344429-97ea6af6-07c9-43ea-be88-f05ea397da01.png)
    
    ![image](https://user-images.githubusercontent.com/238259/215344520-6202b486-e43d-487e-8e6a-bd40e4d95011.png)

--- src/Components/Ticket/TicketsList.tsx
+++ src/Components/Ticket/TicketsList.tsx
@@ -15,23 +15,44 @@ export const TicketsList = ({
 
   return (
     <>
-      {tickets?.map((ticket: OwnTicket) => (
-        <Ticket
-          key={ticket.id}
-          userTicketId={ticket.id}
-          userTicketStatus={ticket.status}
-          userPhoto={user.photo}
-          userUsername={user.username}
-          userName={user.name}
-          ticketName={ticket.ticket.name}
-          ticketType={ticket.ticket.type}
-          ticketSeason={ticket.ticket.season}
-          showSocialLinks={
-            ticket.ticket.type !== "workshop" && ticket.ticket.type !== "meetup"
-          }
-          fadeIn
-        />
-      ))}
+      {tickets?.map((ticket: OwnTicket) => {
+        const ticketOptions =
+          ticket.ticket.type === "workshop"
+            ? {
+                title: "Workshop",
+                subtitle: ticket.ticket.description,
+                selectedTheme: "workshop",
+              }
+            : ticket.ticket.type === "meetup"
+            ? {
+                title: ticket.ticket.name,
+                subtitle: ticket.ticket.description,
+                selectedTheme: "meetup",
+              }
+            : {
+                selectedTheme: "jsconf",
+              };
+
+        return (
+          <Ticket
+            key={ticket.id}
+            title={ticketOptions.title}
+            subtitle={ticketOptions.subtitle}
+            userTicketId={ticket.id}
+            userTicketStatus={ticket.status}
+            userPhoto={user.photo}
+            userUsername={user.username}
+            userName={user.name}
+            ticketName={ticket.ticket.name}
+            ticketType={ticket.ticket.type}
+            ticketSeason={ticket.ticket.season}
+            selectedTheme={
+              ticketOptions.selectedTheme as "workshop" | "meetup" | "jsconf"
+            }
+            fadeIn
+          />
+        );
+      })}
     </>
   );
 };
